243d6e6fd9880538f186235ccc779c32dfcef70f
Divansh Kawatra <divanshkawatra@gmail.com>
2021-04-10 17:51:45 +0530

    BST insert and BFS implementation

--- BST/binarySearchTree.cpp
+++ BST/binarySearchTree.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+// #include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 struct TreeNode{
-    int val; //int data
+    int data;
     TreeNode* left;
     TreeNode* right;
 
     TreeNode(int x){
-        val = x;
+        data = x;
         left = nullptr;
         right = nullptr;
     }
@@ -26,13 +27,57 @@ class BST{
                 root = new TreeNode(x);
             }
             else{
-                
+                TreeNode* temp = root;
+                while(true){
+                    if(temp && x>temp->data){
+                        if(!temp->right){
+                            temp->right = new TreeNode(x);
+                            break;
+                        }
+                        temp = temp->right;
+                    }
+                    if(temp && x<temp->data){
+                        if(!temp->left){
+                            temp->left = new TreeNode(x);
+                            break;
+                        }
+                        temp = temp->left;
+                    }
+                }
             }
         }
 
+        void display(){  // BFS
+            queue<TreeNode*> bfs;
+            bfs.push(root);
 
+            while(!bfs.empty()){
+                int size = bfs.size();
+
+                for(int i=0; i<size; ++i){
+                    TreeNode* front = bfs.front();
+                    bfs.pop();
+                    if(front->left) bfs.push(front->left);
+                    if(front->right) bfs.push(front->right);
+                    cout<<front->data<<" ";
+                }
+            }
+        }
 };
 
 int main(){
+    BST bst;
+    int n;
+    cout<<"Enter the no. of nodes\n";
+    cin>>n;
 
+    for(int i=0; i<n; ++i){
+        int nodeVal;
+        cout<<"Enter node value: ";
+        cin>>nodeVal;
+        bst.insert(nodeVal);
+    }
+
+    bst.display();
+    return 0;
 }
